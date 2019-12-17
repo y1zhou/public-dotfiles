0d08b032a51b3255c86b166e94f905ed2b82185a
y1zhou <zhou.zy.yi@gmail.com>
2019-12-17 17:18:28 +0800

    feat(fish): add scoop and apt proxy support, and switch to exa for ll and la

--- fish/functions/y1zhou_config.fish
+++ fish/functions/y1zhou_config.fish
@@ -2,19 +2,25 @@ abbr  v nvim
 abbr vim nvim
 abbr ... 'cd ../../'
 abbr .... 'cd ../../../'
-abbr la 'ls -trhla --color=always'
+abbr la 'exa -abgFhls new'
+abbr ll 'exa -bgFhls new'
 abbr diff 'diff -y --color=always'
 abbr cdiff 'colordiff -tys'
 
-set -x PATH $HOME/miniconda3/bin $PATH
-set -x PATH /usr/local/go/bin $PATH
+#set -x PATH $HOME/miniconda3/bin $PATH
+#set -x PATH /usr/local/go/bin $PATH
 
 # Taken from: https://blog.skk.moe/post/enable-proxy-on-ubuntu/
 function proxy --description "Enable proxy in WSL"
     curl -s "https://api.ip.sb/geoip" | \
       jq '. | {Status: "oldIP", IP: .ip, Country: .country, Org: .organization}'
-    set -gx ALL_PROXY "socks5://127.0.0.1:9090"
-    set -gx all_proxy "socks5://127.0.0.1:9090"
+
+    set -gx ALL_PROXY "socks5://127.0.0.1:7891"
+    set -gx all_proxy "socks5://127.0.0.1:7891"
+    scoop config proxy "127.0.0.1:7890"
+    echo -e "Acquire::http::Proxy \"http://127.0.0.1:7890\";" | sudo tee -a /etc/apt/apt.conf > /dev/null
+    echo -e "Acquire::https::Proxy \"http://127.0.0.1:7890\";" | sudo tee -a /etc/apt/apt.conf > /dev/null
+
     curl -s "https://api-ipv4.ip.sb/geoip" | \
       jq '. | {Status: "newIPv4", IP: .ip, Country: .country, Org: .organization}'
     curl -s "https://api-ipv6.ip.sb/geoip" | \
@@ -24,6 +30,10 @@ end
 function noproxy --description "Enable proxy in WSL"
     set -e ALL_PROXY 
     set -e all_proxy
+    scoop config rm proxy
+    sudo sed -i -e '/Acquire::http::Proxy/d' /etc/apt/apt.conf
+    sudo sed -i -e '/Acquire::https::Proxy/d' /etc/apt/apt.conf
+
     curl -s "https://api.ip.sb/geoip" | \
       jq '. | {IP: .ip, Country: .country, Org: .organization}'
 end
