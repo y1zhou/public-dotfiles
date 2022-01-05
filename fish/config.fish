5f5bb101c49a13e7b0d78dea50e6882469befc61
y1zhou <17245097+y1zhou@users.noreply.github.com>
2022-01-05 16:14:15 -0500

    fix(fish): colors were off when using together with starship

--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,29 @@
 set -x PATH /home/pi/pkg/bin $PATH
 set -x PATH /home/pi/.cargo/bin $PATH
 
-# Initialize starship prompt
-starship init fish | source
+# Fish syntax highlighting
+# https://github.com/matchai/dotfiles/blob/b6c6a701d0af8d145a8370288c00bb9f0648b5c2/.config/fish/config.fish
+set -g fish_color_autosuggestion '555'  'brblack'
+set -g fish_color_cancel -r
+set -g fish_color_command --bold
+set -g fish_color_comment red
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_end brmagenta
+set -g fish_color_error brred
+set -g fish_color_escape 'bryellow'  '--bold'
+set -g fish_color_history_current --bold
+set -g fish_color_host normal
+set -g fish_color_match --background=brblue
+set -g fish_color_normal normal
+set -g fish_color_operator bryellow
+set -g fish_color_param cyan
+set -g fish_color_quote yellow
+set -g fish_color_redirection brblue
+set -g fish_color_search_match 'bryellow'  '--background=brblack'
+set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
+set -g fish_color_user brgreen
+set -g fish_color_valid_path --underline
 
 # Set neovim as default editor
 set -x VISUAL nvim
@@ -11,3 +32,6 @@ set -x EDITOR nvim
 
 # fzf config using ripgrep
 set -x FZF_DEFAULT_COMMAND  'rg --files --no-ignore-vcs --hidden'
+
+# Initialize starship prompt
+starship init fish | source
