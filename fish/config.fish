a2958941d8b73e7a4711a45b2326e340d0bd4e86
y1zhou <17245097+y1zhou@users.noreply.github.com>
2022-01-31 21:49:44 -0500

    feat (fish): add zoxide to shell config

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 # Local software path
-fish_add_path /home/pi/pkg/bin
-fish_add_path /home/pi/.cargo/bin
+fish_add_path $HOME/pkg/bin
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.cargo/bin
 
 # Fish syntax highlighting
 # https://github.com/matchai/dotfiles/blob/b6c6a701d0af8d145a8370288c00bb9f0648b5c2/.config/fish/config.fish
@@ -32,6 +33,9 @@ set -x EDITOR nvim
 
 # fzf config using ripgrep
 set -x FZF_DEFAULT_COMMAND  'rg --files --no-ignore-vcs --hidden'
+
+# Add zoxide to path
+zoxide init fish | source
 
 # Initialize starship prompt
 starship init fish | source
