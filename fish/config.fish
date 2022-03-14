b98941d990eab1a73ae3aeec0b33a376673915c8
y1zhou <zhou.zy.yi@gmail.com>
2022-03-13 21:14:52 -0400

    feat(fish): use fish_config to set colors instead

--- fish/config.fish
+++ fish/config.fish
@@ -4,27 +4,7 @@ fish_add_path $HOME/.cargo/bin
 
 # Fish syntax highlighting
 # https://github.com/matchai/dotfiles/blob/b6c6a701d0af8d145a8370288c00bb9f0648b5c2/.config/fish/config.fish
-set -g fish_color_autosuggestion '555'  'brblack'
-set -g fish_color_cancel -r
-set -g fish_color_command --bold
-set -g fish_color_comment red
-set -g fish_color_cwd green
-set -g fish_color_cwd_root red
-set -g fish_color_end brmagenta
-set -g fish_color_error brred
-set -g fish_color_escape 'bryellow'  '--bold'
-set -g fish_color_history_current --bold
-set -g fish_color_host normal
-set -g fish_color_match --background=brblue
-set -g fish_color_normal normal
-set -g fish_color_operator bryellow
-set -g fish_color_param cyan
-set -g fish_color_quote yellow
-set -g fish_color_redirection brblue
-set -g fish_color_search_match 'bryellow'  '--background=brblack'
-set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
-set -g fish_color_user brgreen
-set -g fish_color_valid_path --underline
+# Instead, use fish_config theme save 'fish default'
 set -x COLORTERM truecolor
 
 # Set neovim as default editor
